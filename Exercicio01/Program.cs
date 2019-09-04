e18d4c3db79cb0930be7273a4263eb5ff6013211
tatianilima <tatianilima@hotmail.com>
2019-09-04 12:35:56 -0300

    * .gitignore: Exercicio 01 do Curso de C# da Udemy. Treinando   entrada, processamento e saída
    * Exercicio01.sln:
    * Program.cs:
    * Exercicio01.csproj:

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Primeiro declarar as variaveis
+			int valor1, valor2, soma;
+
+			//Depois exibir a janela de entrada de dados
+			Console.WriteLine("Informe o valor 1:");
+			valor1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Informe o valor 2:");
+			valor2 = int.Parse(Console.ReadLine());
+
+			soma = valor1 + valor2;
+            Console.WriteLine("Resultado: ");
+            Console.WriteLine(soma);
+		}
+    }
+}
